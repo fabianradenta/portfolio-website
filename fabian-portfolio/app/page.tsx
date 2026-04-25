e151d132ea3ef1ce17501a812547bc3218ffd6a0
fabianradenta <13522105@mahasiswa.itb.ac.id>
2026-04-25 21:22:41 +0700

    set up nextjs and add navbar

---
+++ fabian-portfolio/app/page.tsx
@@ -0,0 +1,28 @@
+import Navbar from "../components/Navbar";
+import "./globals.css"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+      
+      <div className="max-w-5xl mx-auto">
+        <section id="about" className="min-h-screen flex items-center justify-center border-t border-zinc-800/50 text-zinc-500">
+          <h2>Home Section (Coming Soon)</h2>
+        </section>
+        
+        <section id="about" className="min-h-screen flex items-center justify-center border-t border-zinc-800/50 text-zinc-500">
+          <h2>About Section (Coming Soon)</h2>
+        </section>
+        
+        <section id="projects" className="min-h-screen flex items-center justify-center border-t border-zinc-800/50 text-zinc-500">
+          <h2>Projects Section (Coming Soon)</h2>
+        </section>
+        
+        <section id="others" className="min-h-screen flex items-center justify-center border-t border-zinc-800/50 text-zinc-500">
+          <h2>Others Section (Coming Soon)</h2>
+        </section>
+      </div>
+    </main>
+  );
+}
