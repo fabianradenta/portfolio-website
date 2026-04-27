41cb0f26eed0a09d83252e7a1b64a3a4d22c5b82
fabianradenta <13522105@mahasiswa.itb.ac.id>
2026-04-27 14:03:05 +0700

    add about section

--- fabian-portfolio/app/page.tsx
+++ fabian-portfolio/app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "../components/Navbar";
 import "./globals.css"
 import Hero from "@/components/Hero";
+import BentoAbout from "@/components/AboutBento";
+import Resume from "@/components/Resume";
 
 export default function Home() {
   return (
@@ -9,8 +11,9 @@ export default function Home() {
       
       <div className="max-w-5xl mx-auto">
         <Hero />  
-        <section id="about" className="min-h-screen flex items-center justify-center border-t border-zinc-800/50 text-zinc-500">
-          <h2>About Section (Coming Soon)</h2>
+        <section id="about" className="pt-20">
+          <BentoAbout />
+          <Resume />
         </section>
         
         <section id="projects" className="min-h-screen flex items-center justify-center border-t border-zinc-800/50 text-zinc-500">
